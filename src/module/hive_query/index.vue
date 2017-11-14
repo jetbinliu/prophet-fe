daa4f830e11f8a61945df7988c4850f985f0d7db
jly8866 <164473279@qq.com>
2017-11-14 16:10:32 +0800

    fe debug

--- src/module/hive_query/index.vue
+++ src/module/hive_query/index.vue
@@ -1,13 +1,13 @@
 <template>
     <Row>
         <Col span="5">
-            <Menu active-name="1-2" width="auto" :open-names="['1']" accordion>
+            <Menu active-name="1-2" width="auto" :open-names="['1']" @on-select="descTable" accordion>
                 <Submenu v-for="(tableValue, dbName) in metastoreDbAndTables" :key="dbName" :name="dbName">
                     <template slot="title">
                         <Icon type="soup-can-outline"></Icon>
                             {{dbName}}
                     </template>
-                    <MenuItem v-for="tableLine in tableValue" :key="tableLine.TBL_ID" :name="tableLine.TBL_ID">{{tableLine.TBL_NAME}}
+                    <MenuItem v-for="tableLine in tableValue" :key="tableLine.TBL_ID" :name="tableLine.TBL_ID" >{{tableLine.TBL_NAME}}
 					</MenuItem>
                 </Submenu>
             </Menu>
@@ -16,7 +16,7 @@
             <div class="layout-content-main">
                 <Row>
                     <div style="margin: 10px 10px;">
-                        <Input type="textarea" :rows="6" placeholder="请输入要查询的SQL...查询大表尽量加上分区键和limit"></Input>
+                        <Input type="textarea" :rows="6" placeholder="请输入要查询的SQL...表名请带上db名...查询大表尽量加上分区键和limit"></Input>
                         <div style="margin-top: 10px; float:right;">
                             <Button type="primary">查询(S)</Button>&nbsp;&nbsp;
                             <Button>清空</Button>
@@ -26,24 +26,24 @@
                 <Row>
                     <div style="margin: 10px 10px;">
                         <Tabs type="card" closable @on-tab-remove="handleTabRemove">
-                            <TabPane label="历史查询" v-if="tab0">
+                            <TabPane :label="tabPanels.tab0.label" v-if="tabPanels.tab0.display" name="tab0">
                                 <!-- <Select v-model="model1"  clearable placeholder="请选择城市" @on-change="refresh" style="width:200px">
-                                     <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                                     <Option v-for="item in cityList" value="item.value" :key="item.value">{{ item.label }}</Option>
                                 </Select> -->
                                 <Table size="small" stripe :columns="colHistQuery" :data="dataHistQuery">
-                                    
+									
                                 </Table>
-
                             </TabPane>
-                            <TabPane label="历史结果" v-if="tab1">
+                            <TabPane :label="tabPanels.tab1.label" v-if="tabPanels.tab1.display" name="tab1">
                                 <Table size="small" stripe :columns="columns1" :data="data1">
                                     
                                 </Table>
                                 <router-link to="/home">Go to Foo</router-link>
                             </TabPane>
-                            <TabPane label="标签三" v-if="tab2">标签三的内容</TabPane>
+							<TabPane  v-for="(tabValue, tabKey) in tabPanels" :key="tabKey" :label="'标签' + tabKey" :v-if="tabValue.display && tabKey !='tab0' && tabKey != 'tab1'" :name="tabKey">
+								{{ tabValue.content }}
+							</TabPane>
                         </Tabs>
-
                     </div>
                 </Row>
              </div>
@@ -55,6 +55,16 @@
     export default {
         data () {
             return {
+				tabPanels: {
+					tab0: {label:"历史查询", display:true},
+					tab1: {label:"历史结果", display:true},
+					tab2: {label:"标签1", content:"222", display:true},
+					tab3: {label:"标签2", content:"333", display:true},
+					tab4: {label:"标签3", content:"444", display:true}
+				},
+				tab0: true,
+                tab1: true,
+                tab2: true,
                 cityList: [
                 ],
                 model1: '',
@@ -71,9 +81,6 @@
                     }
                 ],
                 data1: [],
-                tab0: true,
-                tab1: true,
-                tab2: true,
                 colHistQuery: [
                     {
                         title: 'Id',
@@ -103,6 +110,10 @@
             refresh () {
                 this.$Message.info('refresh');
             },
+			descTable() {
+				
+				this.$Message.info("获取表信息成功");
+			},
             getMetaStoreDbTables () {
                 var me = this;
                 getMetaStoreDbAndTables().then(function(res) {
@@ -128,7 +139,9 @@
                 })
             },
             handleTabRemove (name) {
-                this['tab' + name] = false;
+			debugger;
+                //this['tab' + name] = false;
+				this['tabPanels'][name]['display'] = false;
             }
         }
     }
