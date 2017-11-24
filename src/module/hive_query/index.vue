fbee9469603c43fa902e33740d30cb3494633813
jly8866 <164473279@qq.com>
2017-11-24 18:55:27 +0800

    后端查询query结果展现，保存查询历史

--- src/module/hive_query/index.vue
+++ src/module/hive_query/index.vue
@@ -7,7 +7,7 @@
                         <Icon type="soup-can-outline"></Icon>
                             {{dbName}}
                     </template>
-                    <MenuItem v-for="tableLine in tableValue" :key="tableLine.TBL_ID" :name="tableLine.TBL_ID" >{{tableLine.TBL_NAME}}
+                    <MenuItem v-for="tableLine in tableValue" :key="tableLine.TBL_ID" :name="dbName + '.' + tableLine.TBL_NAME" >{{tableLine.TBL_NAME}}
 					</MenuItem>
                 </Submenu>
             </Menu>
@@ -16,32 +16,50 @@
             <div class="layout-content-main">
                 <Row>
                     <div style="margin: 10px 10px;">
-                        <Input type="textarea" :rows="6" placeholder="请输入要查询的SQL...表名请带上db名...查询大表尽量加上分区键和limit"></Input>
+                        <Input v-model="sql_content" type="textarea" :rows="6" 
+							placeholder="请输入要查询的SQL...表名请带上db名...查询大表尽量加上分区键和limit"></Input>
                         <div style="margin-top: 10px; float:right;">
-                            <Button type="primary">查询(S)</Button>&nbsp;&nbsp;
-                            <Button>清空</Button>
+                            <Button type="primary" @click="sendQuery">查询(S)</Button>&nbsp;&nbsp;
+                            <Button @click="clearQuery">清空</Button>
                         </div>
                     </div>
                 </Row>
                 <Row>
                     <div style="margin: 10px 10px;">
-                        <Tabs type="card" closable @on-tab-remove="handleTabRemove">
-                            <TabPane :label="tabPanels.tab0.label" v-if="tabPanels.tab0.display" name="tab0">
+                        <Tabs type="card" @on-tab-remove="handleTabRemove" v-model="currActiveTabPanel">
+                            <TabPane label="历史查询" v-if="tabPanels.tabHistoryQuery.display" name="tabHistoryQuery">
                                 <!-- <Select v-model="model1"  clearable placeholder="请选择城市" @on-change="refresh" style="width:200px">
                                      <Option v-for="item in cityList" value="item.value" :key="item.value">{{ item.label }}</Option>
                                 </Select> -->
                                 <Table size="small" stripe :columns="colHistQuery" :data="dataHistQuery">
 									
                                 </Table>
+								<router-link to="/home">Go to Foo</router-link>
                             </TabPane>
-                            <TabPane :label="tabPanels.tab1.label" v-if="tabPanels.tab1.display" name="tab1">
-                                <Table size="small" stripe :columns="columns1" :data="data1">
-                                    
-                                </Table>
-                                <router-link to="/home">Go to Foo</router-link>
+                            <TabPane label="查询结果" v-if="tabPanels.tabQueryResult.display" name="tabQueryResult">
+								<div>
+									<Table v-if="tabPanels.tabQueryResult.contentType == 'sql_query' && tabPanels.tabQueryResult.status == 0" size="small" 
+										stripe :columns="tabPanels.tabQueryResult.cols" :data="tabPanels.tabQueryResult.data.result_data" 
+										no-data-text="SQL查询返回的结果集为空. 换个查询条件试试？">
+										
+									</Table>
+									<Alert v-if="tabPanels.tabQueryResult.contentType == 'sql_query' && tabPanels.tabQueryResult.status != 0" type="error" show-icon>
+										出错啦!
+										<span slot="desc">
+											{{tabPanels.tabQueryResult.message}}
+										</span>
+									</Alert>
+								</div>
+                                
                             </TabPane>
-							<TabPane  v-for="(tabValue, tabKey) in tabPanels" :key="tabKey" :label="'标签' + tabKey" :v-if="tabValue.display && tabKey !='tab0' && tabKey != 'tab1'" :name="tabKey">
-								{{ tabValue.content }}
+							<TabPane v-for="(tabValue, tabKey) in tabPanels" closable :key="tabKey" :label="tabKey" 
+								v-if="tabValue.display && tabKey !='tabHistoryQuery' && tabKey != 'tabQueryResult'" :name="tabKey">
+								<div>
+									<Table size="small" stripe :v-if="tabValue.contentType == 'desc_table'" :columns="tabValue.cols" 
+										:data="tabValue.content.result_data">
+										
+									</Table>
+								</div>
 							</TabPane>
                         </Tabs>
                     </div>
@@ -51,40 +69,38 @@
      </Row>
 </template>
 <script>
-    import { getMetaStoreDbAndTables } from './request';
+    import { getMetaStoreDbAndTablesAjax } from './request';
+	import { descTableAjax } from './request';
+	import { sendHiveSqlQuery } from './request';
+	import { saveQueryHistory } from './request';
+	
     export default {
         data () {
             return {
+				sql_content: '',
 				tabPanels: {
-					tab0: {label:"历史查询", display:true},
-					tab1: {label:"历史结果", display:true},
-					tab2: {label:"标签1", content:"222", display:true},
-					tab3: {label:"标签2", content:"333", display:true},
-					tab4: {label:"标签3", content:"444", display:true}
+					tabHistoryQuery: {
+						label:"历史查询", 
+						display:true, 
+						status:-1, 
+						message:""
+					},
+					tabQueryResult: {
+						label:"查询结果", 
+						display:true,
+						status:-1,
+						message:"",
+						data:{},
+						cols:[],
+						contentType:""
+					}
 				},
-				tab0: true,
-                tab1: true,
-                tab2: true,
-                cityList: [
-                ],
-                model1: '',
-                columns1: [
-                    {
-                        title: 'userName',
-                        key: 'userName',
-                        sortable: true
-                    },
-                    {
-                        title: 'password',
-                        key: 'password',
-                        sortable: true
-                    }
-                ],
-                data1: [],
+				currActiveTabPanel: "",
                 colHistQuery: [
                     {
                         title: 'Id',
                         key: 'queryId',
+						width: 80
                     },
                     {
                         title: '查询时间',
@@ -99,50 +115,144 @@
                         key: 'status',
                     }
                 ],
-                dataHistQuery: [],
+                dataHistQuery: [
+					
+				],
 				metastoreDbAndTables: {}
             }
         },
         mounted () {
+			this.currActiveTabPanel = "tabHistoryQuery";
             this.getMetaStoreDbTables();
         },
         methods: {
-            refresh () {
-                this.$Message.info('refresh');
-            },
-			descTable() {
-				
-				this.$Message.info("获取表信息成功");
-			},
+			//获取metastore里所有库表信息并展现
             getMetaStoreDbTables () {
                 var me = this;
-                getMetaStoreDbAndTables().then(function(res) {
-                    me.metastoreDbAndTables = res.data;
-                }, function (res) {
-                    me.metastoreDbAndTables = {
-								default112:[
-									{'TBL_ID':21, 'TBL_NAME':'access_path1', 'TBL_TYPE':'EXTERNAL_TABLE'},
-								   {'TBL_ID':22, 'TBL_NAME':'access_path2', 'TBL_TYPE':'EXTERNAL_TABLE'},
-								   {'TBL_ID':23, 'TBL_NAME':'access_path3', 'TBL_TYPE':'MANAGED_TABLE'}
-								 ],
-								formatter:[
-								   {'TBL_ID':24, 'TBL_NAME':'access_path4', 'TBL_TYPE':'EXTERNAL_TABLE'},
-								   {'TBL_ID':25, 'TBL_NAME':'access_path5', 'TBL_TYPE':'EXTERNAL_TABLE'}
-								 ]
-							  };
-                    me.cityList = [
-                        {
-                            value: '1',
-                            label: '北京'
-                        }
-                    ]
-                })
+                getMetaStoreDbAndTablesAjax().then(
+					function (res) {
+						me.metastoreDbAndTables = res.data;
+					}, 
+					function (res) {
+						me.$Message.info("获取所有库表信息失败!");
+					}
+				)
             },
+			//描述表结构信息
+			descTable(menuItemName) {
+				var me = this;
+				var params = {tableNameWithDb : menuItemName};
+				descTableAjax(params).then(
+					function (res) {
+						//ajax成功时执行
+						var columns = new Array();
+						
+						if (res.data.type == 'desc_table') {
+							columns.push({title:"字段名", key:"col_name"});
+							columns.push({title:"字段类型", key:"data_type"});
+							columns.push({title:"描述", key:"comment"});
+						} else {
+							//将后端传来的cols信息拼接成VUE table的columns数组
+							for (var index in res.data.data.result_cols) {
+								var colDict = {
+									title : res.data.data.result_cols[index], 
+									key :   res.data.data.result_cols[index]
+								};
+								columns.push(colDict);
+							}
+						}
+
+						me.$set(
+							me.tabPanels, 
+							menuItemName, 
+							{
+								label:menuItemName, 
+								content:res.data.data, 
+								cols:columns,
+								display:true, 
+								contentType:res.data.type
+							}
+						);
+						me.currActiveTabPanel = menuItemName;
+						me.$Message.info("获取表结构信息成功!");
+					}, 
+					function (res) {
+						//ajax失败时执行
+						me.$Message.error("获取表结构信息失败");
+						me.$Message.error(res.message);
+					}
+				)
+				
+				this.i++;
+			},
+			//close掉每一个结果集选项卡的方法
             handleTabRemove (name) {
-			debugger;
-                //this['tab' + name] = false;
 				this['tabPanels'][name]['display'] = false;
-            }
+            },
+			clearQuery () {
+				this.sql_content = '';
+			},
+			//向后端发送SQL，并异步获取结果后展现
+			sendQuery() {
+				var me = this;
+				var params = {queryContent : this.sql_content};
+				me.$Message.info("SQL发送成功!");
+				
+				//发送query到后端和获取结果的接口
+				sendHiveSqlQuery(params).then(
+					function (res) {
+						//最后切换焦点到当前tab上
+						me.currActiveTabPanel = "tabQueryResult";
+						
+						var columns = new Array();
+						if (res.data.type == 'sql_query' && res.status == 0) {
+							if (res.data.data.result_cols != null) {
+								//如果结果集不为空，将后端传来的cols信息拼接成VUE Table的columns数组
+								if (res.data.data.result_cols.length >= 8) {
+									for (var index in res.data.data.result_cols) {
+										var colDict = {
+											title : res.data.data.result_cols[index], 
+											key :   res.data.data.result_cols[index],
+											width: 120
+										};
+										columns.push(colDict);
+									}
+								} else {
+									for (var index in res.data.data.result_cols) {
+										var colDict = {
+											title : res.data.data.result_cols[index], 
+											key :   res.data.data.result_cols[index]
+										};
+										columns.push(colDict);
+									}
+								}
+							} else {
+								//如果结果集为空啥都不用做
+							}
+							
+							//如果查询成功，则保存一条查询历史
+							saveQueryHistory(params).then(
+								function (res) {
+									me.$Message.info("保存查询历史成功!");
+								},
+								function (res) {
+									me.$Message.warning("保存这条查询历史失败!");
+								}
+							)
+						}
+						me.tabPanels.tabQueryResult.status = res.status;
+						me.tabPanels.tabQueryResult.message = res.message;
+						me.tabPanels.tabQueryResult.data = res.data.data;
+						me.tabPanels.tabQueryResult.cols = columns;
+						me.tabPanels.tabQueryResult.contentType = res.data.type;
+					}, 
+					function (res) {
+						me.$Message.info("请求后端失败！服务器不知道跑哪里玩儿去了....");
+					}
+				)
+				
+			}
+			
         }
     }
 </script>
--- src/module/hive_query/request.js
+++ src/module/hive_query/request.js
@@ -1,7 +1,20 @@
 'use strict';
 
 import { get } from '../../common/util/service';
+import { post } from '../../common/util/service';
 
-export function getMetaStoreDbAndTables(params) {
+export function getMetaStoreDbAndTablesAjax(params) {
 	return get('/hive_query/all_metastore_db_tables.json', params);
+}
+
+export function descTableAjax(params) {
+	return get('/hive_query/desc_table.json', params);
+}
+
+export function sendHiveSqlQuery(params) {
+	return post('/hive_query/send_query.json', params);
+}
+
+export function saveQueryHistory(params) {
+	return post('/hive_query/save_query_history.json', params);
 }
